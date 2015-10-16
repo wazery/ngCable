12e374147b074c35e3c8f4a1d5fb5ca25cfbce7a
joehany <joe.hany@hotmail.com>
2015-10-16 16:57:33 +0200

    Add bower.json file

---
+++ bower.js
@@ -0,0 +1,13 @@
+{
+  "name": "ng-cable",
+  "version": "0.0.1",
+  "main":[
+  	"src/action-cable.js",
+  	"src/ng-cable.js"
+  ],
+  "dependencies": {
+  },
+  "devDependencies": {
+    "angular": "latest"
+  }
+}
