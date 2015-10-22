f9deebf9d8049007d58e5c21e98add3d2ca9aaed
joehany <joe.hany@hotmail.com>
2015-10-22 17:20:07 +0200

    $channel object

--- src/ng-cable.js
+++ src/ng-cable.js
@@ -2,19 +2,48 @@
 
 angular.module('ng-cable', [])
 
-.service('$cable', function () {
+.factory('$cable', function ($channel) {
 
     function AngularCable(url) {
+        if (!(this instanceof AngularCable)) {
+            return new AngularCable(url);
+        }
         this.cable = Cable.createConsumer(url);
-        this.subscriptions = {};
+        this.channels = {};
     }
 
     AngularCable.prototype = {
-        createSubscription: function (channelName, callback) {
-            var subscription = this.cable.subscriptions.create(channelName, callback);
-            this.subscriptions[subscription.name] = subscription;
-            return subscription;
+        subscribe: function (channelName, callback) {
+            var mixin = typeof callback == 'function'? {received: callback}: callback;
+            var channel = this.cable.subscriptions.create(channelName, mixin);
+            //channel = $channel(channel, this)
+            this.channels[channel.name] = channel;
+            return channel;
+        },
+        unsubscribe: function(channelName){
+
         }
     };
     return AngularCable;
-});
+})
+
+.factory('$channel', function(){
+
+    function $channel (baseChannel, $cable) {
+      if (!(this instanceof $channel)) {
+        return new $channel(baseChannel, $cable);
+      }
+
+      this.baseChannel = baseChannel;
+      this.cable = $cable;
+      this.name = baseChannel.name;
+    }
+
+    $channel.prototype = {
+        send: function(){
+
+        }
+    };
+
+    return $channel
+});;
